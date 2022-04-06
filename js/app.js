ba2ae2498dde1a66c9735a323ab7cce3646957c4
JBobznfts <microgrow720@gmail.com>
2022-04-06 14:19:49 +0100

    tes2.0

--- js/app.js
+++ js/app.js
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own a ${COLLECTION_NAME}!! 🌿 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own a ${COLLECTION_NAME} 🌿${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 🌿`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `You don't own a ${COLLECTION_NAME} 😡`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
